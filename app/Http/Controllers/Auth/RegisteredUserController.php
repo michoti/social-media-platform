c05cbc8247ec5dda67c82b2556ee41fd7982b68e
shukeel <triggaillest@gmail.com>
2023-10-30 19:43:02 +0300

    feat(db) designed the draft db

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,14 +31,18 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users,username'],           
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'profile_image' => [ 'string'],
+            'bio' => [ 'string'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'profile_image' => $request->profile_image,
+            'bio' => $request->bio,
             'password' => Hash::make($request->password),
         ]);
 
